e504518ae92773db332b2e8132f51bd92fa27f75
mjdave542-svg <mjdave542@gmail.com>
2026-01-19 09:58:01 +0530

    Add files via upload

---
+++ #Q-1.py
@@ -0,0 +1,27 @@
+#Q-1 
+print("Welcome to the Python Programming World!")
+
+#Q-2
+print("rajesh kumar")
+print("\tflat no.101,sunshine apartments")
+print("\tMG Road,sector 15")
+print("\trajkot")
+print("\tpincode:360005")
+print("\tindia")
+     
+#Q-3
+a=150 
+b=120.50
+     
+print("addition =",a+b)
+print("subtraction =",a-b)
+print("multiplication =",a*b)
+print("division =",a/b)
+
+#Q-4
+radius=7
+area=3.14*radius*radius
+circumference=2*3.14*radius
+
+print("area of circle =",area)
+print("circumference of circle =",circumference)
