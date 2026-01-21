b7ddb7c0c82684e5fd6d043f920bd6fdcbef216f
mjdave542-svg <mjdave542@gmail.com>
2026-01-21 07:43:50 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,85 @@
+#Q-1
+print("Welcome to Python Programming")
+
+
+#Q-2
+print("Rajesh Kumar")
+print("\tFlat No. 101, Sunshine Apartments")
+print("\tMG Road, Sector 15")
+print("\tRajkot")
+print("\tPincode: 360004")
+print("\tIndia")
+
+
+#Q-3
+a = 150
+b = 120.50
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+
+
+#Q-4
+r = 7
+area = 3.14 * r * r
+circumference = 2 * 3.14 * r
+
+print("Area of Circle =", area)
+print("Circumference of Circle =", circumference)
+
+
+#Q-5
+P = 1000
+R = 5
+T = 2
+
+SI = (P * R * T) / 100
+print("Simple Interest =", SI)
+
+
+#Q-6
+length = 10
+width = 5
+
+perimeter = 2 * (length + width)
+print("Perimeter of Rectangle =", perimeter)
+
+
+#Q-7
+length = 10
+width = 5
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("Area of Rectangle =", area)
+print("Perimeter of Rectangle =", perimeter)
+
+
+#Q-8
+a = 3
+b = 4
+c = 5
+
+perimeter = a + b + c
+print("Perimeter of Triangle =", perimeter)
+
+
+#Q-9
+side = 6
+
+area = side * side
+perimeter = 4 * side
+
+print("Area of Square =", area)
+print("Perimeter of Square =", perimeter)
+
+
+#Q-10
+side = 6
+
+perimeter = 4 * side
+print("Perimeter of Square =", perimeter)
+
