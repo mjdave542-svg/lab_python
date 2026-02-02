006e7daa00e67cbd69e1ed8dad8715efabff6c3c
mjdave542-svg <mjdave542@gmail.com>
2026-02-02 10:46:19 +0530

    Add files via upload

---
+++ i o.py
@@ -0,0 +1,32 @@
+#1
+a=("hello world")
+print(a)
+
+b=["welcome world"]
+print(b)
+
+#2
+int=input("enter no:-")
+print("the no is",int)
+
+int=input("enter no:-")
+print("the no is",int)
+
+#3
+x=5
+y=6
+
+print("the sum of x and y :",x+y)
+
+c=10
+d=10
+print("the sum of x and y :",c*d)
+
+#4
+e=9
+f=3
+print(e>f)
+
+g=10
+h=11
+print(g<h)
