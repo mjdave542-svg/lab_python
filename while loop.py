6c67ecdfb653c130b5c3ca68fbb9f3394f258972
mjdave542-svg <mjdave542@gmail.com>
2026-02-25 08:33:26 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,20 @@
+#Q-1 
+i=1
+while i<=5:
+   print(i)
+   i=i+1
+   
+#Q-2
+total_sum=0
+num=int(input("enter a number to add(or 0 to finish):"))
+while num !=0:
+    total_sum += num
+    num=int(input("enter a number to add(or 0 to finish):"))
+    print("the total sum is:",total_sum)
+ 
+ #Q-3
+for i in range(1,21):
+     if i % 2 !=0:
+         print(i)
+         
+#Q-4
